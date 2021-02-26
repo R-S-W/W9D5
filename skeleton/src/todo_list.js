89a6e609531fa44c905e705ab211be47e9c13d73
Andrew Kim <andrewikim95@gmail.com>
2021-02-26 14:26:46 -0500

    started on todolist

--- skeleton/src/todo_list.js
+++ skeleton/src/todo_list.js
@@ -1,2 +1,8 @@
+const todos = [];
 
+const list = getElementsByClassName('todos');
+const form = getElementsByClassName('add-todo-form');
 
+function addTodo(){
+    let addTodoInput = document.querySelector
+};
